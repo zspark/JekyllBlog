bd14b7dd50ca83f2e3cbc8c64e9bd8a39fd46560
Jerry Chaos <z_spark@163.com>
2016-09-20 00:46:33 +0800

    big change

--- archive_time.markdown
+++ archive_time.markdown
@@ -1,6 +1,6 @@
 ---
 layout: page
-title: Time
+title: Archieve(time)
 permalink: /time/
 ---
 
--- webgl/merge.bat
+++
@@ -1,1 +0,0 @@
-copy .\main.js+.\cases\casebase.js+.\cases\c1_framework.js+.\cases\c2_triangle.js+.\cases\c3_rotation.js .\main.min.js /b
